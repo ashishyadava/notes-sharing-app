37b342e6cd834f68085f894c6973cc0ac906df16
Ashish <ashishyadava1996@gmail.com>
2024-12-04 18:23:52 +0530

    add frontend project

--- app.js
+++ app.js
@@ -17,42 +17,46 @@ app.use(session({secret: "cats", resave: false, saveUninitialized: false}));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false}));
 app.use(methodOverride('_method')); 
+app.use(express.static('public'));
 
 
-
-  passport.use(
-    new LocalStrategy(async (username, password, done) => {
-      try {
-        const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
-        const user = rows[0];
-        if (!user) {
-          return done(null, false, { message: "Incorrect username" });
-        }
-        const match = await bcrypt.compare(password, user.password);
-        if (!match) {
-          return done(null, false, { message: "Incorrect password" });
-        }
-        return done(null, user);
-      } catch(err) {
-        return done(err);
-      }
-    })
-  );
-
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
-  
-  passport.deserializeUser(async (id, done) => {
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
     try {
-      const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
       const user = rows[0];
-  
-      done(null, user);
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+
+      console.log("Entered password: " + password);
+      console.log("Encrypted password: " + user.password)
+
+      return done(null, user);
     } catch(err) {
-      done(err);
+      return done(err);
     }
-  });
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+  
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
   
 
 
